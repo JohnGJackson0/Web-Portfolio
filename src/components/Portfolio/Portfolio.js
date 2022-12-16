0394ddbd5863bff9143bc4352a6938143a58331a
johngjackson0 <johngjackson0@gmail.com>
2022-12-16 07:50:18 -0500

    refactor: popup boxes

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -10,10 +10,6 @@ import GoStat7 from "../../images/GOSTAT7.png";
 import GoStat8 from "../../images/GOSTAT8.png";
 import GoStat9 from "../../images/GOSTAT9.png";
 import GoStat10 from "../../images/GOSTAT10.png";
-import GoStat11 from "../../images/GOSTAT11.png";
-import GoStat12 from "../../images/GOSTAT12.png";
-import GoStat13 from "../../images/GOSTAT13.png";
-import GoStat14 from "../../images/GOSTAT14.png";
 import MyNutrition1 from "../../images/MYNUTRITION1.png";
 import MyNutrition2 from "../../images/MYNUTRITION2.png";
 import MyNutrition3 from "../../images/MYNUTRITION3.png";
@@ -30,191 +26,17 @@ import Staty2 from "../../images/STATY2.png";
 import Staty3 from "../../images/STATY3.png";
 import Staty4 from "../../images/STATY4.png";
 import Staty5 from "../../images/STATY5.png";
-import { FaSearchPlus } from "react-icons/fa";
-import { IconContext } from "react-icons";
-import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import { PopupboxContainer } from "react-popupbox";
 import "react-popupbox/dist/react-popupbox.css";
 import { PortfolioImageBox } from "./PortfolioImageBox";
+import {
+  openpopupboxStaty,
+  openpopupboxGoDo,
+  openpopupboxGoStat,
+  openpopupboxMyNutrition,
+} from "./PopupBoxes";
 
 const Portfolio = () => {
-  const openpopupboxStaty = () => {
-    const content = (
-      <div>
-        <img
-          className="portfolio-image-popupbox"
-          src={Staty1}
-          alt="Staty - Statistics App"
-        />
-        <p>
-          App created in the Flutter.
-          <br />
-          Get this app on IOS and Android!
-        </p>
-        <a
-          href=""
-          className="hyper-link"
-          onClick={() => {
-            window.open(
-              "https://play.google.com/store/apps/details?id=me.johnjackson.staty&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-            );
-          }}
-        >
-          Android
-        </a>
-        <br />
-        <a
-          href=""
-          className="hyper-link"
-          onClick={() => {
-            window.open(
-              "https://apps.apple.com/us/app/staty-statistics-calculator/id1658300476"
-            );
-          }}
-        >
-          Ios
-        </a>
-        <p>See the source code:</p>
-        <a
-          href=""
-          className="hyper-link"
-          onClick={() => {
-            window.open("https://github.com/JohnGJackson0/staty");
-          }}
-        >
-          Github
-        </a>
-      </div>
-    );
-    PopupboxManager.open({
-      content,
-      config: {
-        titleBar: {
-          enable: true,
-          text: "Staty - Statistics App",
-        },
-        fadeIn: true,
-        fadeInSpeed: 500,
-      },
-    });
-  };
-
-  const openpopupboxGoDo = () => {
-    const content = (
-      <div>
-        <img
-          className="portfolio-image-popupbox"
-          src={GoDo3}
-          alt="GoDo - Task Manager"
-        />
-        <p>
-          App created in the React Native framework.
-          <br />
-          Launched on Google Play store and soon IOS.
-        </p>
-        <b>Android/Google Play:</b>
-        <a
-          href=""
-          className="hyper-link"
-          onClick={() => {
-            window.open(
-              "https://play.google.com/store/apps/details?id=com.tanagr.godo"
-            );
-          }}
-        >
-          https://play.google.com/store/apps/details?id=com.tanagr.godo
-        </a>
-      </div>
-    );
-
-    PopupboxManager.open({
-      content,
-      config: {
-        titleBar: {
-          enable: true,
-          text: "GoDo - Task Manager",
-        },
-        fadeIn: true,
-        fadeInSpeed: 500,
-      },
-    });
-  };
-
-  const openpopupboxGoStat = () => {
-    const content = (
-      <div>
-        <img className="portfolio-image-popupbox" src={GoStat2} alt="goStat" />
-        <p>
-          App created in the native Android SDK.
-          <br />
-          This is an inference statistics calculator for students learning
-          elementary statistics.
-        </p>
-        <b>GitHub:</b>
-        <a
-          href=""
-          className="hyper-link"
-          onClick={() => {
-            window.open("https://github.com/jgjio/GoStat");
-          }}
-        >
-          https://github.com/jgjio/GoStat
-        </a>
-      </div>
-    );
-
-    PopupboxManager.open({
-      content,
-      config: {
-        titleBar: {
-          enable: true,
-          text: "Go Stats",
-        },
-        fadeIn: true,
-        fadeInSpeed: 500,
-      },
-    });
-  };
-
-  const openpopupboxMyNutrition = () => {
-    const content = (
-      <div>
-        <img
-          className="portfolio-image-popupbox"
-          src={MyNutrition3}
-          alt="My Nutrition"
-        />
-        <p>
-          App created in the React Native framework.
-          <br />
-          This is a calorie counter for people trying to gain, maintain or lose
-          weight.
-        </p>
-        <b>GitHub:</b>
-        <a
-          href=""
-          className="hyper-link"
-          onClick={() => {
-            window.open("https://github.com/jgjio/my-nutrition");
-          }}
-        >
-          https://github.com/jgjio/my-nutrition
-        </a>
-      </div>
-    );
-
-    PopupboxManager.open({
-      content,
-      config: {
-        titleBar: {
-          enable: true,
-          text: "My Nutrition",
-        },
-        fadeIn: true,
-        fadeInSpeed: 500,
-      },
-    });
-  };
-
   return (
     <div id="portfolio" className="portfolio-wrapper">
       <div className="container">
