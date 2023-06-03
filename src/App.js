40f075f20128e331bd312b9514f229510b8a204f
johngjackson0 <johngjackson0@gmail.com>
2023-06-03 09:58:01 -0400

    fix: particles no longer work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/AboutMe";
@@ -13,27 +12,6 @@ import ContactMe from "./components/ContactMe";
 function App() {
   return (
     <>
-      <Particles
-        className="particles-canvas"
-        params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
-                enable: true,
-                value_area: 900,
-              },
-            },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 6,
-                color: "#f9ab00",
-              },
-            },
-          },
-        }}
-      />
       <Navbar />
       <Header />
       <About />
